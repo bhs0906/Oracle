d002385f571265dc76b95a5c7ab345a9581ef001
bhs0906 <hg5112@naver.com>
2022-06-16 20:29:49 +0900

    220616

---
+++ 과제/과제4/hyun.sql
@@ -0,0 +1,39 @@
+SELECT TEAM_ID AS 팀ID
+     , TO_NUMBER(ZIP_CODE1, '999') + TO_NUMBER(ZIP_CODE2, '999') AS 우편번호합
+FROM TEAM;
+
+SELECT ename
+    , CASE
+        WHEN SAL > 2000 THEN SAL
+        ELSE 2000
+    END AS REVISED_SALARY
+FROM EMP;
+
+SELECT loc
+    , CASE LOC
+        WHEN 'NEW YORK' THEN 'EAST'
+        WHEN 'BOSTON'   THEN 'EAST'
+        WHEN 'CHICAGO'  THEN 'CENTER'
+        WHEN 'DALLAS'   THEN 'CENTER'
+        ELSE 'ETC'
+    END AS AREA
+FROM DEPT;
+
+SELECT ename
+    , CASE
+        WHEN SAL >= 3000 THEN 'HIGH'
+        WHEN SAL >= 1000 THEN 'MID'
+        ELSE 'LOW'
+    END AS SALARY_GRADE
+FROM EMP;
+
+SELECT ename, sal
+    , CASE
+        WHEN SAL >=2000 THEN 1000
+        
+        ELSE(CASE
+                WHEN SAL>=1000 THEN 500
+                ELSE 0
+            END)
+        END AS BOUNS
+FROM EMP;
